56a51dca444a155b8cbdf1607c5d711ed30b31f4
DiegoIpaez <diegokpo1313@gmail.com>
2022-03-12 04:08:37 -0300

    sockets escuchando los msj

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,34 @@
-const socket = io()
+const socket = io();
+
+//Referencias del HTML
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
+
+socket.on("connect", () => {
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  lblOnline.style.display = "none";
+  lblOffline.style.display = "";
+});
+
+socket.on("enviar-mensaje", (payload) => {
+    console.log(payload)
+})
+
+btnEnviar.addEventListener("click", () => {
+  const mensaje = txtMensaje.value;
+  const payload = {
+      mensaje,
+      id: '1232',
+      fecha: new Date().getTime()
+  }
+
+  socket.emit('enviar-mensaje', payload, ( id )=>{
+      console.log('Desde el server', id)
+  })
+});
