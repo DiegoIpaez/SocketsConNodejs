c7907eeb8d2af66f3a676089e35c6507ec586717
DiegoIpaez <diegokpo1313@gmail.com>
2022-03-12 03:05:58 -0300

    socket.io added

---
+++ models/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    //le asigno a app los metodos de express
+    this.app = express();
+    this.port = process.env.PORT;
+    this.server = require("http").createServer(this.app);
+    this.io = require("socket.io")(this.server);
+
+    //Paths
+    this.paths = {};
+
+    //Middlewares
+    this.middlewares();
+    //Rutas
+    this.routes();
+    //Sockets
+    this.sockets();
+  }
+
+  middlewares() {
+    //Public
+    this.app.use(express.static("public"));
+    //Cors
+    this.app.use(cors());
+    // //Parser
+    // this.app.use(express.json());
+  }
+
+  routes() {
+    // this.app.use(this.paths.auth, require("../routes/auth"));
+  }
+
+  sockets() {
+    this.io.on("connection", (socket) => {
+      console.log("Cliente conectado", socket.id);
+
+      socket.on("disconnect", () => {
+        console.log("Cliente desconectado", socket.id);
+      });
+    });
+  }
+
+  //Se monto el servidor en el port
+  listen() {
+    this.server.listen(this.port, () =>
+      console.log("Servidor en el puerto", this.port)
+    );
+  }
+}
+
+module.exports = Server;
